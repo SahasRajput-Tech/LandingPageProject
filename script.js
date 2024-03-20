736f7a1d2faa11c61c8bc4ee25097f21fd6f4b7b
sahasrajput.tech <sahasrajput.tech@gmail.com>
2024-03-21 00:16:37 +0530

    I made changes in the code of JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function enroll(){
-    alert("You have successfully enrolled in the course");
-    alert("Congratulations And Happy Learning!")
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const enrollButtons = document.querySelectorAll('.course button');
+    
+    enrollButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        enrollCourse(this.parentNode);
+      });
+    });
+    
+    function enrollCourse(courseElement) {
+      const courseName = courseElement.querySelector('h3').textContent;
+      // Here you can add further logic like sending an AJAX request to enroll the user in the course
+      alert(`You have successfully enrolled in ${courseName}!`);
+    }
+  });
+  
