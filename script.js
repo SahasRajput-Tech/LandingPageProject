e59529159163b6a69e3cf2dee0e3bedd5936feec
sahasrajput.tech <sahasrajput.tech@gmail.com>
2024-04-14 23:50:53 +0530

    modified code

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const enrollButtons = document.querySelectorAll('.course button');
-    
-    enrollButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        enrollCourse(this.parentNode);
-      });
-    });
-    
-    function enrollCourse(courseElement) {
-      const courseName = courseElement.querySelector('h3').textContent;
-      // Here you can add further logic like sending an AJAX request to enroll the user in the course
-      alert(`You have successfully enrolled in ${courseName}!`);
-    }
-  });
-  
+// JavaScript function to handle enrollment
+function enroll() {
+  alert("You have successfully enrolled in the course!");
+}
